a99d64d7e6f3c66efbded1cf6555438406330835
83metra <jumo_004@mail.ru>
2024-09-19 21:07:03 +0200

    Update Module 11 3.py
    Строка 25 - забыты скобки. Добавлены.

--- Module 11 3.py
+++ Module 11 3.py
@@ -22,7 +22,7 @@ def introspection_info(obj):
     object_info = {}
     if inspect.isfunction(obj) or inspect.isclass(obj):
         object_info.update({'Имя объекта': obj.__name__})
-    if inspect.isfunction(obj) and obj.__name__ == 'wrapper' or 'surrogate':
+    if inspect.isfunction(obj) and (obj.__name__ == 'wrapper' or 'surrogate'):
         object_info.update({'Тип объекта': f'{type(obj)} (скорее всего функция-декоратор)'})
     else:
         object_info.update({'Тип объекта': type(obj)})
@@ -48,9 +48,9 @@ class Info:
 # obj = introspection_info(45)
 # obj = introspection_info('Ratio')
 # obj = introspection_info(Info)
-# obj = introspection_info({1,2,54,3467,2322,'Я множество!', 342, [})
+obj = introspection_info({1,2,54,3467,2322,'Я множество! Поклоняйтесь мне!', 342, })
 # obj = introspection_info(print_info)
-obj = introspection_info((a, a**2) for a in range(100))
+# obj = introspection_info((a, a**2) for a in range(100))
 # obj = introspection_info(introspection_info)
 
 #pprint(obj)
